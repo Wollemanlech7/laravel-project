2ee4b9b76a916efb518ce43cfcea29c44ef6e29e
Arturo <magaarturordaz180@gmail.com>
2021-03-23 20:34:39 -0600

    table for subject and structure of index

--- resources/views/boxoffice/content/subjects/index.blade.php
+++ resources/views/boxoffice/content/subjects/index.blade.php
@@ -15,15 +15,10 @@
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Subjects
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Exam
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Status
                                 </th>
-                                <th scope="col" class="relative px-6 py-3">
-                                    <span class="sr-only">Edit</span>
-                                </th>
+                                
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
@@ -47,9 +42,7 @@
                                     <div class="text-sm text-gray-900">Solving linear equations and linear inequalities</div>
                                     <div class="text-sm text-gray-500">Hearth of Algebra</div>
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    SAT
-                                </td>
+                            
                                 <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
                                     <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                 </td>
@@ -76,9 +69,7 @@
                                     <div class="text-sm text-gray-900">Solving linear equations and linear inequalities</div>
                                     <div class="text-sm text-gray-500">Hearth of Algebra</div>
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    SAT
-                                </td>
+                               
                                 <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
                                     <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                 </td>
@@ -105,9 +96,7 @@
                                     <div class="text-sm text-gray-900">Solving linear equations and linear inequalities</div>
                                     <div class="text-sm text-gray-500">Hearth of Algebra</div>
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    SAT
-                                </td>
+                              
                                 <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
                                     <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                 </td>
